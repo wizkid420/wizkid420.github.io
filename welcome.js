73c8465739a263c94c9e87be028707b6fcf5a6fc
wizkid420 <131621188+wizkid420@users.noreply.github.com>
2023-04-23 18:58:53 -0300

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to My Website!</h1>
+      <p>Thanks for stopping by!</p>
+    </div>
+  );
+}
+
+export default Welcome;
